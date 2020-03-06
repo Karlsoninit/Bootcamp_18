1ffed253f8100cd1ad722d4be85162e39bfde1b8
Karlson <m.norets@gmail.com>
2020-03-06 13:27:56 +0200

    create todo with material_ua

---
+++ todo_ui/src/components/form/Form.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import shortID from "shortid";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const initialState = {
+  name: "",
+  todo: "",
+  description: ""
+};
+
+const styles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: 200
+    }
+  }
+}));
+
+class Form extends Component {
+  state = {
+    ...initialState
+  };
+
+  handleSubmitForm = event => {
+    event.preventDefault();
+    // console.log(event.target);
+    // const formData = new FormData(event.target);
+    // const user = {};
+    // formData.forEach((value, name) => (user[name] = value));
+    // console.log(user);
+
+    this.props.onGetValue({
+      ...this.state,
+      id: shortID(),
+      date: new Date().toDateString()
+    });
+    this.setState({
+      ...initialState
+    });
+  };
+
+  handleChangeValue = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    // const classes = styles();
+    const { name, todo, description } = this.state;
+    return (
+      <form
+        // className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={this.handleSubmitForm}
+      >
+        <TextField
+          onChange={this.handleChangeValue}
+          type="text"
+          name="name"
+          value={name}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+        <TextField
+          onChange={this.handleChangeValue}
+          type="text"
+          name="todo"
+          value={todo}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+        <TextField
+          onChange={this.handleChangeValue}
+          type="text"
+          name="description"
+          value={description}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+
+        <button type="submit">SUBMIT</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
