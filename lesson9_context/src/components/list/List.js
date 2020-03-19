beab2a238488963ff08bae74bbee043bc227c76b
Karlson <m.norets@gmail.com>
2020-03-19 09:06:31 +0200

    default assembly

---
+++ lesson9_context/src/components/list/List.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./list.module.css";
+//принимает notes и перебирает
+const List = () => <div className={styles.notesContainer}></div>;
+
+export default List;
