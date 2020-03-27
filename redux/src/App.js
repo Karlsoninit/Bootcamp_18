6685e569c3f9ffb8f16e4ccde052866a9b36edde
Karlson <m.norets@gmail.com>
2020-03-27 09:40:04 +0200

    default redux assembly

---
+++ redux/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Notes from "./components/notes/Notes";
+
+function App() {
+  return (
+    <>
+      <Notes />
+    </>
+  );
+}
+
+export default App;
---
+++ redux/src/components/form/Form.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import styles from "./form.module.css";
+
+class Form extends Component {
+  state = {
+    note: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <form onSubmit={this.handleSubmit} noValidate autoComplete="off">
+          <TextField
+            className={styles.input}
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            name="note"
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
